10a9b2db4e757f0a4aa69aec91294c92e2e42e16
Tetiana Topchii <tetiana.topchii@performance-media.pl>
2023-04-18 19:26:00 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style/app.scss';
+import Nav from './components/Nav'
+import Intro from './components/Intro';
+import { useTranslation } from 'react-i18next';
+import About from './components/About';
+import Ourgames from './components/Ourgames';
+import Video from './components/Video';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Nav
+     about={t("about")}
+     ourgames={t("ourgames")}
+     contact ={t("contact")}
+     changeEN ={()=>changeLanguage("en")}
+     changePL ={()=>changeLanguage("pl")}/>
+     <Intro title={t("title")}/>
+     <About aboutTitle={t("aboutTitle")}
+            aboutText={t('aboutText')}/>
+     <Ourgames titleGames={t('titleGames')}
+                textGames={t('textGames')}
+                text2Games={t("text2Games")}/>
+      <Video/>
+      <Contact titleContact={t("titleContact")}
+               inputName = {t("inputName")}
+               inputEmail = {t("inputEmail")}
+               inputMessage = {t("inputMessage")}
+               send = {t("send")}/>
+       <Footer about={t("about")}
+               ourgames={t("ourgames")}
+               contact ={t("contact")}
+               teams = {t("teams")}
+               polis = {t("polis")}/>       
+    </>
   );
 }
 
